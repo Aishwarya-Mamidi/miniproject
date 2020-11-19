5426dacc1af8283f5bd3a5da7dfa656a471fb04e
Aishwarya-Mamidi <mamidiaishwarya25014@gmail.com>
2020-11-19 15:40:16 +0530

    styling left

--- getplaced/urls.py
+++ getplaced/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('',views.first,name='first'),
@@ -30,5 +31,8 @@ urlpatterns=[
     path('studprofile/',views.studprofile,name='studprofile'),
     path('cprofile/',views.cprofile,name='cprofile'),
     path('viewprofile/',views.viewprofile,name='viewprofile'),
-
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password" ),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_complete.html"), name="password_reset_complete"),
 ]
